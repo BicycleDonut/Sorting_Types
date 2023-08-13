ba637956c4186d55d545435b7063425685412790
Alexander Ryzhov <aryzhov84@gmail.com>
2023-08-13 17:13:19 +0300

    formating files

--- Sorting types/src/sort.java
+++ Sorting types/src/sort.java
@@ -6,37 +6,30 @@ public class sort {
         // bubbleSort(array);
         // insertSort(array);
 
-
         for (int i = 0; i < array.length; i++) {
             System.out.print(array[i] + "  ");
         }
     }
 
+    // Простые алгоритмы сортировки O(n^2)
+    // Сортировка массива пузырьком
+
     public static void bubbleSort(int[] array) {
-
-
         boolean finish;
         do {
             finish = true;
-
-
             for (int i = 0; i < array.length - 1; i++) {
-
                 if (array[i] > array[i + 1]) {
-
                     int temp = array[i];
                     array[i] = array[i + 1];
                     array[i + 1] = temp;
                     finish = false;
-
                 }
-
             }
-
         } while (!finish);
-
     }
 
+    // Сортировка массива выбором
 
     public static void directSort(int[] array) {
         for (int i = 0; i < array.length - 1; i++) {
@@ -51,10 +44,10 @@ public class sort {
                 array[i] = array[minPosition];
                 array[minPosition] = temp;
             }
-
         }
-
     }
+
+    // Сортировка массива вставками
 
     public static void insertSort(int[] array) {
         for (int i = 0; i < array.length - 1; i++) {
